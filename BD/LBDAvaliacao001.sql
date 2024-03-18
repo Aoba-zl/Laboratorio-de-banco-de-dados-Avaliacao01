8720da58acae3d26e4a8fddc0bcc352ceefb97a8
Matheus França <matheussss6613@gmail.com>
2024-03-18 00:52:46 -0300

    add diagrama, banco, procedure - cpf

---
+++ BD/LBDAvaliacao001.sql
@@ -0,0 +1,113 @@
+USE master
+
+DROP DATABASE matricula
+
+CREATE DATABASE matricula
+GO
+USE matricula
+GO
+CREATE TABLE aluno
+(
+	ra								CHAR(9) 		NOT NULL,
+	cpf								CHAR(11) 		NOT NULL,
+	nome							VARCHAR(100) 	NOT NULL,
+	nome_social						VARCHAR(100)	NOT NULL,
+	dt_nascimento					DATE			NOT NULL,
+	email_pessoal					VARCHAR(100)	NOT NULL,
+	email_corporativo				VARCHAR(100)	NOT NULL,
+	dt_conclusao_seg_grau			DATE			NOT NULL,
+	instituicao_conclusao_seg_grau	VARCHAR(100)	NOT NULL
+	PRIMARY KEY (ra)
+)
+GO
+CREATE TABLE telefone
+(
+	ra_aluno						CHAR(9) 		NOT NULL,
+	numero							CHAR(9)			NOT NULL
+	PRIMARY KEY (ra_aluno)
+	FOREIGN KEY (ra_aluno) REFERENCES aluno (ra)
+)
+GO
+CREATE TABLE vestibular
+(
+	ra_aluno						CHAR(9)			NOT NULL,
+	pontuacao						DECIMAL(7, 2)	NOT NULL,
+	posicao							INT				NOT NULL
+	PRIMARY KEY (ra_aluno)
+	FOREIGN KEY (ra_aluno) REFERENCES aluno (ra)
+)
+GO
+
+CREATE TABLE curso
+(
+	codigo							INT				NOT NULL,
+	nome							VARCHAR(50)		NOT NULL,
+	carga_horaria					TIME			NOT NULL,
+	sigla							VARCHAR(30)		NOT NULL,
+	ult_nota_participacao_enade		DECIMAL(7, 1)	NOT NULL
+	PRIMARY KEY (codigo)
+)
+GO
+CREATE TABLE matricula
+(
+	id								INT				NOT NULL,
+	ra_aluno						CHAR(9)			NOT NULL,
+	codigo_curso					INT				NOT NULL,
+	semestre						BIT				NOT NULL,
+	semestre_ingresso				BIT				NOT NULL,
+	ano_limite_graduacao			DATE			NOT NULL,
+	ano_ingresso					DATE			NOT NULL
+	PRIMARY KEY (id)
+	FOREIGN KEY (ra_aluno) REFERENCES aluno (ra),
+	FOREIGN KEY (codigo_curso) REFERENCES curso (codigo)
+)
+GO
+CREATE TABLE funcionario
+(
+	id								INT				NOT NULL,
+	nome							VARCHAR(100)	NOT NULL,
+	cpf								CHAR(11)		NOT NULL,
+	cargo							VARCHAR(50)		NOT NULL
+	PRIMARY KEY (id)
+)
+GO
+CREATE TABLE professor
+(
+	id_funcionario					INT				NOT NULL,
+	titulacao						VARCHAR(30)		NOT NULL,
+	especialidade					VARCHAR(30)		NOT NULL
+	PRIMARY KEY (id_funcionario)
+	FOREIGN KEY (id_funcionario) REFERENCES funcionario (id)
+)
+GO
+CREATE TABLE disciplina
+(
+	codigo							INT				NOT NULL,
+	codigo_curso					INT				NOT NULL,
+	nome							VARCHAR(50)		NOT NULL,
+	qntd_hora_semanais				INT				NOT NULL,
+	id_funcionario_professor		INT				NOT NULL
+	PRIMARY KEY (codigo)
+	FOREIGN KEY (codigo_curso) REFERENCES curso (codigo),
+	FOREIGN KEY (id_funcionario_professor) REFERENCES professor (id_funcionario)
+)
+GO
+CREATE TABLE conteudo
+(
+	id								INT				NOT NULL,
+	codigo_disciplina				INT				NOT NULL,
+	nome							VARCHAR(50)		NOT NULL,
+	descricao						VARCHAR(100)	NOT NULL
+	PRIMARY KEY (id)
+	FOREIGN KEY (codigo_disciplina) REFERENCES disciplina (codigo)
+)
+
+
+
+
+
+
+
+
+
+
---
+++ BD/Procedure/LBDCPF.sql
@@ -0,0 +1,69 @@
+USE LBDExerc006
+
+DROP PROCEDURE sp_verifica_cpf
+
+CREATE PROCEDURE sp_verifica_cpf 
+(
+	@cpf CHAR(11), 
+	@cpf_valido INT OUTPUT
+)
+AS
+	DECLARE @cpf_num_iguais CHAR(11),
+			@cpf_count INT = 10,
+			@cpf_soma INT = 0,
+			@cpf_unidade INT = 1,
+			@cpf_ult_digito VARCHAR(2) = ''
+	SET @cpf_num_iguais = CONCAT(SUBSTRING(@cpf, 1, 1), REPLACE(@cpf, SUBSTRING(@cpf, 1, 1), ''))
+	IF (LEN(@cpf_num_iguais) != 1)
+	BEGIN
+		WHILE (@cpf_count >= 2)
+		BEGIN
+			SET @cpf_soma = @cpf_soma + (@cpf_count * CAST(SUBSTRING(@cpf, @cpf_unidade, 1) AS INT))
+			SET @cpf_count = @cpf_count - 1
+			SET @cpf_unidade = @cpf_unidade + 1
+		END
+		IF (@cpf_soma % 11 = 1 OR @cpf_soma % 11 = 0)
+		BEGIN 
+			SET @cpf_ult_digito = CONCAT(@cpf_ult_digito, '0')
+		END
+		ELSE 
+		BEGIN 
+			SET @cpf_ult_digito = CONCAT(@cpf_ult_digito, CAST((11 - (@cpf_soma % 11)) AS VARCHAR))
+		END
+		SET @cpf_count = 11
+		SET @cpf_soma = 0
+		SET @cpf_unidade = 1
+		WHILE (@cpf_count >= 2)
+		BEGIN
+			IF (@cpf_unidade <= 9)
+			BEGIN 
+				SET @cpf_soma = @cpf_soma + (@cpf_count * CAST(SUBSTRING(@cpf, @cpf_unidade, 1) AS INT))
+			END
+			ELSE 
+			BEGIN 
+				SET @cpf_soma = @cpf_soma + (@cpf_count * CAST(@cpf_ult_digito AS INT))
+			END
+			SET @cpf_count = @cpf_count - 1
+			SET @cpf_unidade = @cpf_unidade + 1
+		END
+		IF (@cpf_soma % 11 = 1 OR @cpf_soma % 11 = 0)
+		BEGIN 
+			SET @cpf_ult_digito = CONCAT(@cpf_ult_digito, '0')
+		END
+		ELSE 
+		BEGIN 
+			SET @cpf_ult_digito = CONCAT(@cpf_ult_digito, CAST((11 - (@cpf_soma % 11)) AS VARCHAR))
+		END
+		IF (SUBSTRING(@cpf, 10, 2) = @cpf_ult_digito)
+		BEGIN 
+			SET @cpf_valido = 0 --cpf valido
+		END
+		ELSE 
+		BEGIN 
+			SET @cpf_valido = 1 --cpf invalido
+		END
+	END
+	ELSE
+	BEGIN
+		SET @cpf_valido = 2 --cpf não pode com numeros iguais
+	END
